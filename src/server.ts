70e70d35982d88d54d04d14b6aac86e1ad7e5020
John Tucker <jtucker@appirio.com>
2019-02-01 12:41:18 -0500

    Port text

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,4 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => res.send({ hello: 'world' }));
-app.listen(PORT, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
